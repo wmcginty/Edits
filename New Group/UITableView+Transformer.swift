2ad3a74564cde292a7359b731e9e064b95807a16
Will McGinty <mcgintw@gmail.com>
2017-09-25 22:33:51 -0500

    Add usage of performBatchUpdates on table view

--- New Group/UITableView+Transformer.swift
+++ New Group/UITableView+Transformer.swift
@@ -10,6 +10,13 @@ import UIKit
 
 public extension UITableView {
     
+    @available(iOS 11, *)
+    func processUpdates<U>(for transformer: Transformer<U>, in section: Int, completion: ((Bool) -> Void)? = nil) {
+        performBatchUpdates({
+            transformer.editSteps.forEach { $0.edit(forSection: section, in: self) }
+        }, completion: completion)
+    }
+    
     func processUpdates<U>(for transformer: Transformer<U>, in section: Int) {
         beginUpdates()
         transformer.editSteps.forEach { $0.edit(forSection: section, in: self) }
